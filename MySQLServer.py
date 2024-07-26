a52efeacac03805cef654fbd8fe85ec2fa605dda
Bildad58 <bildadwasioya@gmail.com>
2024-07-26 07:35:30 +0300

    initialcommit

---
+++ MySQLServer.py
@@ -0,0 +1,29 @@
+import mysql.connector
+
+# connects MySQlServer to to alx_book_store database
+
+try:
+
+    mydb = mysql.connector.connect(
+    host="localhost",
+    user="root",
+    password="password",
+    database="mydb"
+    )
+
+    mycursor = mydb.cursor()
+
+    mycursor.execute("CREATE DATABASE alx_book_store")
+    print("Database 'alx_book_store' was created successful")
+
+except mysql.ConnectionError:
+    print ("Unable to connect to the database")
+
+# Close connections
+mycursor.close()
+mydb.close()
+
+
+
+
+
