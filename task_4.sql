347f0ccfc59e120c96ae36278ce382ef438d5729
Bildad58 <bildadwasioya@gmail.com>
2024-07-27 05:26:08 +0300

    update

--- task_4.sql
+++ task_4.sql
@@ -2,8 +2,9 @@
 USE alx_book_store;
 
 #select all books in the database
+SELECT
 INFORMATION_SCHEMA.COLUMNS",
  "COLUMN_NAME", 
  "COLUMN_TYPE", 
- "TABLE_SCHEMA  = 'alx_book_store'",
+ "TABLE_SCHEMA = 'alx_book_store'",
  "TABLE_NAME = 'Books'";
