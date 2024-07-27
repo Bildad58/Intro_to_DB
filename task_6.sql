eddd9df4efa8280e5202db6963ddc6fbf3baa8a7
Bildad58 <bildadwasioya@gmail.com>
2024-07-27 06:48:14 +0300

    update

--- task_6.sql
+++ task_6.sql
@@ -12,7 +12,7 @@ def Create record(customer_id,customer_name,email,address):
 Query = "INSERT INTO customer (customer_id,customer_name,email,address) VALUES (%s,%s,%S,%s)"
 Values = (customer_id,customer_name,email,address)
 my_cursor.execute(Query,values)
-Create record("2","Blessing Malik","bmalik@sandtech.com","24 Happiness  Ave.")
+Create record("2","Blessing Malik","bmalik@sandtech.com","124 Happiness  Ave.")
 Create record("3","Obed Ehoneah","eobed@sandtech.com","125 Happiness  Ave.")
 Create record("3","Nehemial Kamolu","nkamolu@sandtech.com","126 Happiness Ave.")
 
