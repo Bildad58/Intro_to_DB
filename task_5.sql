2ad1a2289160c3fca7a024cc628165b4f9a5478a
Bildad58 <bildadwasioya@gmail.com>
2024-07-27 06:07:17 +0300

    update

--- task_5.sql
+++ task_5.sql
@@ -11,7 +11,9 @@ def create record(customer_id,customer_name,email,address):
 Query = "INSERT INTO customer (customer_id,customer_name,email,address) VALUES(%s,%s,%s,%s)"
 Values = (customer_id,customer_name,email,address)
 my_cursor.execute(Query,Values)
+create record("1","Cole Baidoo","cbaidoo@sandtech.com","23 Happiness Ave")
 mydatabe.commit()
+
 
 my_cursor.close()
 mydatabase.close()
